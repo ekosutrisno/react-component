63ecd18d9aa8f7ed8b359e540e21e2074d63ca5b
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2020-08-14 14:29:46 +0700

    Fix Layout Card

--- src/pages/ButtonsView.js
+++ src/pages/ButtonsView.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {
    ButtonDefault,
-   ButtonDefaultShadow,
+   // ButtonDefaultShadow,
    ButtonPrimary,
-   ButtonPrimaryShadow,
+   // ButtonPrimaryShadow,
    ButtonLink,
    ButtonText
 } from '../components/shared/Button';
@@ -166,6 +166,57 @@ export class ButtonsView extends Component {
                   <button className="fa fa-fw fa-code fa-lg focus:outline-none hover:text-gray-700"></button>
                </div>
             </div>
+            {/* Card 05 */}
+            <div className="flex-col justify-start h-full w-70 bg-white rounded-lg shadow-md">
+               <div className="header h-16 p-6 font-semibold">
+                  Mulitple Button
+               </div>
+               <div className="image max-h-full p-6 border-t border-gray-300">
+                  <div className="flex flex-wrap justify-evenly items-center">
+                     <div className="py-3 w-full">
+                        {/* <ButtonBlock primary btnText="Primary Button 2" /> */}
+                        For Mulitple Button Section
+                     </div>
+                  </div>
+               </div>
+               <div className="descriptions h-auto p-6 border-t border-gray-300">
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab ullam dolorum eum rem,
+                  dicta dignissimos id, eaque sint dolore sed maiores itaque ducimus qui,
+                  velit unde quo nobis libero a.</p>
+               </div>
+               <div className="footer flex items-center justify-end h-16 p-6 border-t border-gray-300 space-x-4">
+                  <button className="fab fa-fw fa-github fa-lg focus:outline-none hover:text-gray-700"></button>
+                  <button className="fa fa-fw fa-vector-square fa-lg focus:outline-none hover:text-gray-700"></button>
+                  <button className="far fa-fw fa-clone fa-lg focus:outline-none hover:text-gray-700"></button>
+                  <button className="fa fa-fw fa-code fa-lg focus:outline-none hover:text-gray-700"></button>
+               </div>
+            </div>
+            {/* Card 06 */}
+            <div className="flex-col justify-start h-full w-70 bg-white rounded-lg shadow-md">
+               <div className="header h-16 p-6 font-semibold">
+                  Loading Button
+               </div>
+               <div className="image max-h-full p-6 border-t border-gray-300">
+                  <div className="flex flex-wrap justify-evenly items-center">
+                     <div className="py-3 w-full">
+                        {/* <ButtonBlock primary btnText="Primary Button 2" /> */}
+                        For Loading Button
+                     </div>
+                  </div>
+               </div>
+               <div className="descriptions h-auto p-6 border-t border-gray-300">
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab ullam dolorum eum rem,
+                  dicta dignissimos id, eaque sint dolore sed maiores itaque ducimus qui,
+                  velit unde quo nobis libero a.</p>
+               </div>
+               <div className="footer flex items-center justify-end h-16 p-6 border-t border-gray-300 space-x-4">
+                  <button className="fab fa-fw fa-github fa-lg focus:outline-none hover:text-gray-700"></button>
+                  <button className="fa fa-fw fa-vector-square fa-lg focus:outline-none hover:text-gray-700"></button>
+                  <button className="far fa-fw fa-clone fa-lg focus:outline-none hover:text-gray-700"></button>
+                  <button className="fa fa-fw fa-code fa-lg focus:outline-none hover:text-gray-700"></button>
+               </div>
+            </div>
+
          </div>
       );
    }
--- src/pages/CardsView.js
+++ src/pages/CardsView.js
@@ -4,19 +4,19 @@ import CardMansony from '../components/cards/CardMansony';
 export class CardsView extends Component {
    render() {
       return (
-         <div className="container mx-auto px-4 md:px-48 my-4">
-            <CardMansony title="CardMansony 01" src="http://lorempixel.com/640/480/business" />
-            <div className="flex pt-4 flex-wrap">
+         <div className="container mx-auto my-4">
+            <CardMansony title="CardMansony 01" src="https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=300" />
+            <div className="flex pt-4 flex-wrap lg:space-x-6">
                <CardMansony
                   small
                   title="CardMansony 02"
-                  src="http://lorempixel.com/640/480/people"
+                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                   imgHeight="64" />
 
                <CardMansony
                   small
                   title="CardMansony 03"
-                  src="http://lorempixel.com/640/480/nightlife"
+                  src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                   imgHeight="32"
                />
             </div>
